00683cb6942a591c8709f25fc7b554a750a15def
Shailendra652 <47470233+Shailendra652@users.noreply.github.com>
2023-07-28 21:54:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+
+  const name = "Shailendra ";
+  return (
+
+    
+    <div>
+      <h1>Hello {name?
+      <> if block runs</>:
+      <><h1>else condition</h1>
+      <h1>another block</h1></>
+      }</h1>
+      <p>Start editing to see some magic happen :)</p>
+       
+    </div>
+  );
+}
